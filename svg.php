d6cd8747770a442a40f0a34eaa449165e932f8cd
Anibal C <detops.tk@gmail.com>
2015-09-28 19:10:18 -0500

    create SVG images  AND extra

---
+++ svg.php
@@ -0,0 +1,74 @@
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 1000 1000"
+		style="background-color:blue">
+
+		<rect width="100%" height="100%" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+    <!-- SVG content -->
+    <image xlink:href="public/image/peru-mapa.png" x="200" y="0" height="80%" width="80%"/>
+
+	<!--lima-->
+	<path
+"	onclick="seleccionarDepa(this)"
+     style="fill:yellow;stroke:#ffffff;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+     d="m 326.53602,365.59852 c 1.02106,0.25526 3.41446,4.17298 4.23728,4.50211 3.69855,1.47942 4.1877,14.34226 7.81251,16.15467 5.40496,3.74186 3.68602,8.89937 5.77983,10.05586 4.27685,2.36229 1.01765,-0.54048 5.61733,8.54285 0.0779,0.15385 3.36381,2.15688 4.5431,2.32848 0.51639,0.0751 5.81295,-7.24556 5.70363,-6.63937 -0.14057,0.77952 8.25066,-7.2603 7.65441,-11.95387 -0.35178,-2.76915 -17.99264,-51.39584 -44.65847,-34.24603 -0.38195,3.84253 1.4339,9.60885 3.31038,11.2553 z"
+     id="departamento_02"
+	 metadatajson="{name:'ANCASH',sector:[{name: 1, image: 'public/image/sectores/ico_transporte_xx.png'}]}"
+     inkscape:connector-curvature="0"
+     transform="matrix(1.25,0,0,1.25,0,-125)"
+     sodipodi:nodetypes="cscssssscc" />
+
+
+	<text id="nombre_depa" x="350" y="500" fill="black" style="font-weight:bold; font-size: 19px" >Name</text>
+
+	<!-- cirlces-->
+	<!-- TRANSPORTE-->
+	<image
+	id="sector_1"
+	xlink:href="public/image/sectores/ico_transporte.png" x="50" y="0" height="100px" width="100px"/>
+
+	<!-- COMERCIO-->
+	<image
+	id="sector_2"
+	xlink:href="public/image/sectores/ico_comercio.png" x="50" y="100" height="100px" width="100px"/>
+
+	<!-- IRRIGACION-->
+	<image
+	id="sector_3"
+	xlink:href="public/image/sectores/ico_irrigacion.png" x="50" y="200" height="100px" width="100px"/>
+
+	<!-- ENERGIA-->
+	<image
+	id="sector_4"
+	xlink:href="public/image/sectores/ico_energia.png" x="50" y="300" height="100px" width="100px"/>
+
+	<!-- SEGURIDAD-->
+	<image
+	id="sector_5"
+	xlink:href="public/image/sectores/ico_seguridad.png" x="50" y="400" height="100px" width="100px"/>
+
+	<!-- EDUCACION-->
+	<image
+	id="sector_6"
+	xlink:href="public/image/sectores/ico_educacion.png" x="50" y="500" height="100px" width="100px"/>
+
+	<!-- SANEAMIENTO SALUD-->
+	<image
+	id="sector_7"
+	xlink:href="public/image/sectores/ico_salud.png" x="50" y="600" height="100px" width="100px"/>
+
+	<!-- TELECOMUNICACIONES-->
+	<image
+	id="sector_8"
+	xlink:href="public/image/sectores/ico_telecomunicaciones.png" x="50" y="700" height="100px" width="100px"/>
+
+	<!-- CULTURA-->
+	<image
+	id="sector_9"
+	xlink:href="public/image/sectores/ico_cultura.png" x="50" y="800" height="100px" width="100px"/>
+
+	<!-- OTROS-->
+	<image
+	id="sector_10"
+	xlink:href="public/image/sectores/ico_otros.png" x="50" y="900" height="100px" width="100px"/>
+
+
+</svg>
