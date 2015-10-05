621ceedea451fad4ced7e7eabafeeb21085ee0fc
Anibal C <detops.tk@gmail.com>
2015-10-05 03:35:23 -0500

    style svg limpiar codigo

--- svg.php
+++ svg.php
@@ -1,11 +1,8 @@
 <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 1000 850">
-	<rect width="1000" height="850" style="fill:#00ffff;stroke-width:3;stroke:#00ff00"></rect>
-
-    <!-- SVG content -->
+	<rect width="1000" height="850" style="fill:#ffffff;stroke-width:3;stroke:#00ff00"></rect>
     <image xlink:href="public/image/peru-mapa.png" x="300" y="40" height="700" width="700"/>
 
-	<!--lima-->
-  <path
+	<path
 	onmouseover="seleccionarDepa(this, true)"
 	onmouseout="seleccionarDepa(this, false)"
 	onclick="clickDepa(this)"
@@ -17,101 +14,86 @@
 	 	sectores: {
 	 		1: {
 	 			name: 'Transporte',
-	 			content: '<ol><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento Jr. Progreso Coischo – Santa<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento calles Huarmey<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Acceso vial Nuevo Chimbote – Santa<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Construcción y mejoramiento calles Huarmey<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento camino vecinal Huarmey -Ancash<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento vías Caserío de Chora, Lacabamba<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento pistas y veredas barrios de Huaraz<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento infraestructura vial y peatonal distritos de Huaraz<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento infraestructura vial barrio La Pradera, Huaraz<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento Jr. Ladislao Meza, Huaraz<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento infraestructura vial y peatonal Llamac, Pacllon<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento infraestructura vial y peatonal Pacllon<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento carretera vecinal Culebras, Huarmey<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento pistas y veredas San Cristóbal de Chupan, Huachis<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento Jr. Progreso Coischo – Santa<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento Jr. Progreso Coischo – Santa<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento Jr. Progreso Coischo – Santa<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento Jr. Progreso Coischo – Santa<\/a><\/li><\/ol>',
-	 			image: 'public/image/sectores/ico_transporte_on.png',
-	 			imageSelect: 'public/image/sectores/ico_transporte_xx.png'
+	 			content: '<ol><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento Jr. Progreso Coischo – Santa<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento calles Huarmey<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Acceso vial Nuevo Chimbote – Santa<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Construcción y mejoramiento calles Huarmey<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento camino vecinal Huarmey -Ancash<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento vías Caserío de Chora, Lacabamba<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento pistas y veredas barrios de Huaraz<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento infraestructura vial y peatonal distritos de Huaraz<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento infraestructura vial barrio La Pradera, Huaraz<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento Jr. Ladislao Meza, Huaraz<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento infraestructura vial y peatonal Llamac, Pacllon<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento infraestructura vial y peatonal Pacllon<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento carretera vecinal Culebras, Huarmey<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento pistas y veredas San Cristóbal de Chupan, Huachis<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Pistas y veredas Chasquitambo - Colquioc<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Pistas y veredas Llamarumi - Colquioc\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Pistas y veredas Llampa - Colquioc<\/a><\/li><li><a onclick=\'abrirVentanaHijo(event)\' href=\'#\'>Mejoramiento transitabilidad Ciudad del Pescador – Huarmey<\/a><\/li><\/ol>',
 	 		},
 	 		2: {
 	 			name: 'Comercio y Turismo',
-	 			image: 'public/image/sectores/ico_comercio_on.png',
-	 			imageSelect: 'public/image/sectores/ico_comercio_xx.png'
 	 		},
 	 		6: {
 	 			name: 'Educación',
-	 			image: 'public/image/sectores/ico_educacion_on.png',
- 				imageSelect: 'public/image/sectores/ico_educacion_xx.png'
 	 		},
 	 		7: {
 	 			name: 'Salud',
-	 			image: 'public/image/sectores/ico_salud_on.png',
-	 			imageSelect: 'public/image/sectores/ico_salud_xx.png'
 	 		},
 	 		9: {
 	 			name: 'Cultura y Esparcimiento',
-	 			image: 'public/image/sectores/ico_cultura_on.png',
-	 			imageSelect: 'public/image/sectores/ico_cultura_xx.png'
 	 		}
 	 	}
 	}"
-
-     style="fill:yellow;fill-opacity:0;stroke:#ffffff;stroke-width:1.65666521px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+	style="fill:yellow;fill-opacity:0;stroke:#ffffff;stroke-width:1.65666521px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
 	d="m 511.59938,366.36084 c 1.52701,0.35234 2.22962,4.99698 2.97623,6.00766 0.70775,0.95805 2.84112,0.95771 3.21833,3.23246 0.70491,4.25095 2.44314,5.57341 3.28837,10.07707 0.44758,2.38478 1.21014,8.55456 2.60045,9.19619 8.14113,6.97806 8.72551,11.09803 11.49642,12.42452 0.53197,0.25466 1.17218,-3.59233 2.12215,-3.59517 0.90762,-0.003 1.55745,0.53753 2.40655,0.45655 0.86354,-0.0825 -1.4017,1.63471 -0.5273,2.57336 1.30868,1.40485 4.32966,3.22952 7.10864,4.62 0.77225,0.10369 6.46742,-0.73325 7.18883,-1.26196 3.09195,-2.26605 -0.62591,-4.6842 1.96143,-6.59297 2.20491,-1.62665 8.54848,-3.55916 8.13822,-6.54003 -0.10026,-0.72835 -1.94598,-2.37715 -3.69348,-6.58137 -0.82957,-1.99579 0.74874,-4.07453 -0.41506,-6.53341 -0.81094,-1.71337 4.6631,-7.13129 3.6562,-8.97649 -0.67283,-1.23304 4.7464,0.31996 3.94014,-0.91447 -0.46927,-0.71847 -0.0741,-2.06035 -0.60487,-2.78254 -1.21291,-1.65014 -4.06161,-3.62257 -5.60031,-5.26004 -2.63241,-2.80142 -3.67657,-5.53023 -6.99871,-8.00173 -2.4599,-1.83003 -3.53116,-6.52912 -6.30187,-8.05001 -0.75412,-0.41394 -0.73815,-0.59945 -1.51895,-0.96997 -1.43208,-0.67957 0.3327,-2.16045 -0.62895,-3.37501 -1.33937,-1.69169 -2.624,-3.6019 -4.82527,-4.37004 -1.58565,-0.55332 -2.69608,0.98692 -4.61649,0.86151 -2.18709,-0.14282 -6.20598,0.13046 -7.74877,1.9547 -2.2098,2.61294 -5.39996,4.85689 -6.6746,6.53541 -1.60181,2.1093 -1.96401,5.63956 -2.87757,6.96572 -1.55466,2.25678 -3.89736,1.18695 -7.09149,3.08304 -0.57121,5.30401 -2.78456,3.54437 0.0217,5.81702 z"
      id="path2983"
      inkscape:connector-curvature="0"
      sodipodi:nodetypes="cssscsssscsssssssssssssssssscc" />
-
-
 
 	<text id="nombre_depa" x="0" y="0" fill="black" style="font-weight:bold; font-size: 15px;" />
 
 	<!-- cirlces-->
 	<!-- TRANSPORTE-->
 	<image id="sector_1" xlink:href="" x="380" y="0" height="100px" width="100px"/>
-	<text id="sector_text_1" x="240" y="50" fill="#BBBDBF" style="font-size: 23px;font-weight:bold;">A</text>
+	<text id="sector_text_1" x="240" y="50" fill="#BBBDBF" style="font-size: 23px;font-weight:bold;"></text>
 
 	<!-- COMERCIO-->
 	<image
 	id="sector_2"
 	xlink:href="" x="300" y="65" height="100px" width="100px"/>
-	<text id="sector_text_2" x="70" y="110" fill="#BBBDBF" style="font-size: 23px;font-weight:bold;">B</text>
+	<text id="sector_text_2" x="70" y="110" fill="#BBBDBF" style="font-size: 23px;font-weight:bold;"></text>
 
 	<!-- IRRIGACION-->
 	<image
 	id="sector_3"
 	xlink:href="" x="240" y="145" height="100px" width="100px"/>
-	<text id="sector_text_3" x="120" y="200" fill="#BBBDBF" style="font-size: 23px;font-weight:bold;">C</text>
+	<text id="sector_text_3" x="120" y="200" fill="#BBBDBF" style="font-size: 23px;font-weight:bold;"></text>
 
 	<!-- ENERGIA-->
 	<image
 	id="sector_4"
 	xlink:href="" x="200" y="230" height="100px" width="100px"/>
-	<text id="sector_text_4" x="110" y="285" fill="#BBBDBF" style="font-size: 23px;font-weight:bold;">D</text>
+	<text id="sector_text_4" x="110" y="285" fill="#BBBDBF" style="font-size: 23px;font-weight:bold;"></text>
 
 	<!-- SEGURIDAD-->
 	<image
 	id="sector_5"
 	xlink:href="" x="190" y="325" height="100px" width="100px"/>
-	<text id="sector_text_5" x="70" y="385" fill="#BBBDBF" style="font-size: 23px;font-weight:bold;">E</text>
+	<text id="sector_text_5" x="70" y="385" fill="#BBBDBF" style="font-size: 23px;font-weight:bold;"></text>
 
 	<!-- EDUCACION-->
 	<image
 	id="sector_6"
 	xlink:href="" x="200" y="420" height="100px" width="100px"/>
-	<text id="sector_text_6" x="85" y="480" fill="#BBBDBF" style="font-size: 23px;font-weight:bold;">F</text>
+	<text id="sector_text_6" x="85" y="480" fill="#BBBDBF" style="font-size: 23px;font-weight:bold;"></text>
 
 	<!-- SANEAMIENTO SALUD-->
 	<image
 	id="sector_7"
 	xlink:href="" x="230" y="515" height="100px" width="100px"/>
-	<text id="sector_text_7" x="0" y="575" fill="#BBBDBF" style="font-size: 23px;font-weight:bold;">G</text>
+	<text id="sector_text_7" x="0" y="575" fill="#BBBDBF" style="font-size: 23px;font-weight:bold;"></text>
 
 	<!-- TELECOMUNICACIONES-->
 	<image
 	id="sector_8"
 	xlink:href="" x="280" y="600" height="100px" width="100px"/>
-	<text id="sector_text_8" x="40" y="665" fill="#BBBDBF" style="font-size: 23px;font-weight:bold;">H</text>
+	<text id="sector_text_8" x="40" y="665" fill="#BBBDBF" style="font-size: 23px;font-weight:bold;"></text>
 
 	<!-- CULTURA-->
 	<image
 	id="sector_9"
 	xlink:href="" x="350" y="670" height="100px" width="100px"/>
-	<text id="sector_text_9" x="65" y="735" fill="#BBBDBF" style="font-size: 23px;font-weight:bold;">I</text>
+	<text id="sector_text_9" x="65" y="735" fill="#BBBDBF" style="font-size: 23px;font-weight:bold;"></text>
 
 	<!-- OTROS-->
 	<image
 	id="sector_10"
 	xlink:href="" x="440" y="720" height="100px" width="100px"/>
-	<text id="sector_text_10" x="365" y="795" fill="#BBBDBF" style="font-size: 23px;font-weight:bold;">J</text>
-
-
+	<text id="sector_text_10" x="365" y="795" fill="#BBBDBF" style="font-size: 23px;font-weight:bold;"></text>
 </svg>
